e529b931c0905a35501b1ead586205d0eb17e890
BwanaQ <thunjawax@gmail.com>
2021-04-10 15:28:11 +0300

    create custom user

--- nyumbaX/models.py
+++ nyumbaX/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.utils import timezone
+from users.models import User
 # Create your models here.
+
+
+class Business(models.Model):
+    title = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
